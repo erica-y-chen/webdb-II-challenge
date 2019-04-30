dc2484b09479329b54d3cf78aebbd58e1762431f
erica-y-chen <eyufanchen@gmail.com>
2019-04-30 14:19:28 -0700

    completed mvp

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-
-const knexConfig = {
-  client: 'sqlite3', 
-  connection: {
-    filename: './data/lambda.sqlite3',
-  },
-  useNullAsDefault: true, 
-}
-
-const db = knex(knexConfig); 
-
-const server = express();
-
-server.use(express.json());
-server.use(helmet());
 
 // endpoints here
-
+const server = require('./api/server.js')
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
